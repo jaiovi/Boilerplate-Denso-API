0e58c425f1965830677135c43d13eb13a5fad54c
A01411625 <74069437+A01411625@users.noreply.github.com>
2022-04-06 00:54:59 -0500

    Update user_service.py

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -2,6 +2,7 @@ from src.models.user import User
 from src.models.resultado import Resultado
 from src.models.session import Session
 from database import db
+import re
 
 class UserServices:
     @staticmethod
@@ -18,8 +19,11 @@ class UserServices:
     @staticmethod
     def create_user(name, email, password, validate_password, last_name, role, location, managerPerm): #nos falta age
 
+        if email != "^\w+(@denso.com)$":
+            return {"message":"Su dirección de correo no tiene los permisos para acceder.", "success":False}, 400
+
         if password != validate_password:
-            return {"message":"Las contraseñas son diferentes", "success":False}, 400
+            return {"message":"Las contraseñas son diferentes.", "success":False}, 400
          
         #new_user = User(name=name, email=email, password=password)
         new_user = User(name=name, email=email, password=password, last_name=last_name, role=role, location=location, managerPerm=managerPerm) #nos falta age
